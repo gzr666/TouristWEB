da0f87dcdc062c04fef2c3e9c7b232f85e8d183e
gzr <bleksabat666@gmail.com>
2016-05-03 18:35:42 +0200

    changed googlefonts

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,11 +1,21 @@
 (function(){
 
 angular.module("routerApp")
-.controller("HomeController",function($scope,$rootScope)
+.controller("HomeController",function($scope,$rootScope,$http,apartmanService)
 
 	{
 		$rootScope.linker = 0;
 		$rootScope.pageClass = 'page-home';
+
+
+		apartmanService.getApartman(1).then(function(data){
+	
+		
+	
+	},function(error){
+	
+	
+	});
 
 		
     
